87addea6fdce494c198a220c6354af4b91acfac9
Nolli Manuele <manuele.nolli@supsi.ch>
2023-11-02 16:32:06 +0100

    Updated create wallet function

--- blockchain/scripts/createWallet.ts
+++ blockchain/scripts/createWallet.ts
@@ -1,5 +1,5 @@
 import {generateSalt} from "./utils/genericUtils";
-import {EventLog, Log, ZeroAddress} from "ethers";
+import {EventLog, ZeroAddress} from "ethers";
 import {WalletFactory} from "../typechain-types";
 import {getENVValue} from "./utils/env/envConfig";
 import {ethers} from "hardhat";
@@ -11,11 +11,11 @@ import {ethers} from "hardhat";
  * @param guardian address
  * @param manager address
  */
-export async function createWallet(walletFactory: WalletFactory, owner: string, guardian: string, manager: string) {
+export async function createWallet(walletFactory: WalletFactory, owner: string, guardian: string, manager: string, baseWalletAddress: string) {
 
     const tx = await walletFactory.connect(await ethers.provider.getSigner(manager)).createCounterfactualWallet(
         owner,
-        [getENVValue("BASE_WALLET_ADDRESS")],
+        [baseWalletAddress],
         guardian,
         await generateSalt(),
         0,
@@ -23,7 +23,6 @@ export async function createWallet(walletFactory: WalletFactory, owner: string, 
         "0x",
         "0x"
     )
-
     const receipt = await tx.wait();
     const events = receipt?.logs[4];
     if (events instanceof EventLog) { // TODO: check if this is the right way to do it, I tried with listener but it didn't work
