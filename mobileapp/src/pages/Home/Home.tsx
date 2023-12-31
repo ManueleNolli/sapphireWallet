f6e6780cc30e4a99c58887499b2dbaed4ef1dc74
Nolli Manuele <manuele.nolli@supsi.ch>
2023-12-31 10:54:28 +0100

    Cleared home styles

--- mobileapp/src/pages/Home/Home.tsx
+++ mobileapp/src/pages/Home/Home.tsx
@@ -1,14 +1,18 @@
-import React, { useMemo, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import {
-  Button,
   Layout,
   Text,
   StyleService,
   useStyleSheet,
 } from '@ui-kitten/components'
-import { ScrollView, TouchableWithoutFeedback, View } from 'react-native'
+import {
+  Animated,
+  ScrollView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native'
 import { ImageBackground } from 'expo-image'
-import { homeBackground } from '../../assets/AssetsRegistry'
 import { appStyles, vh, vw } from '../../Styles'
 import { BlurView } from 'expo-blur'
 import { formatBlockchainAddress } from '../../utils/formatBlockchainData'
@@ -23,81 +27,64 @@ export default function Home({ navigation }: any) {
   } = useHome()
   const styles = useStyleSheet(themedStyles)
 
-  const scrollViewRef = useRef<ScrollView>(null)
-  const [heightImageContainer, setHeightImageContainer] = useState(60 * vh)
-
-  const imageContainerStyle = useMemo(() => {
-    return [styles.imageContainer, { height: heightImageContainer }]
-  }, [heightImageContainer, styles.imageContainer])
-
-  const handleScroll = (event: any) => {
-    const offset = event.nativeEvent.contentOffset.y
-    //cast to int to avoid type error
-
-    console.log('offset')
-    // if (offset > 0) {
-    // navigation.setOptions({
-    //   headerTransparent: false,
-    // })
-    // }
-    // cast to number to avoid type error
-
-    setHeightImageContainer(60 * vh - offset)
-  }
-
   return (
     <Layout style={{ flex: 1 }}>
-      <ScrollView
-        ref={scrollViewRef}
-        onScroll={handleScroll}
-        scrollEventThrottle={1} // Adjust the throttle value as needed
-      >
-        <View style={imageContainerStyle}>
-          <ImageBackground
-            source={backgroundImage}
-            contentFit={'cover'}
-            style={appStyles.center}
-          >
-            <View style={styles.balanceContainer}>
-              <BlurView intensity={20} style={styles.balanceBlurContainer}>
-                <Text style={styles.balanceText} category="h4">
-                  {balance} ETH
-                </Text>
-              </BlurView>
-            </View>
-          </ImageBackground>
-        </View>
-        <View>
-          <View style={styles.addressContainer}>
-            <TouchableWithoutFeedback
-              onPress={copyAddressToClipboard}
-              style={{ flex: 1 }}
-            >
-              <BlurView style={appStyles.center} intensity={100}>
-                <Text category="label">
-                  {formatBlockchainAddress(getWalletContractAddress())}
-                </Text>
-              </BlurView>
-            </TouchableWithoutFeedback>
+      <View style={styles.imageContainer}>
+        <ImageBackground
+          source={backgroundImage}
+          contentFit="cover"
+          style={styles.imageBackground}
+        >
+          <View style={styles.balanceContainer}>
+            <BlurView intensity={20} style={styles.balanceBlurContainer}>
+              <Text style={styles.balanceText} category="h4">
+                {balance} ETH
+              </Text>
+            </BlurView>
           </View>
+        </ImageBackground>
 
-          <View
-            style={{
-              width: '100%',
-              height: 400,
-              marginTop: 4 * vh,
-            }}
-          ></View>
+        <View style={styles.addressContainer}>
+          <TouchableWithoutFeedback
+            onPress={copyAddressToClipboard}
+            style={{ flex: 1 }}
+          >
+            <BlurView style={appStyles.center} intensity={100}>
+              <Text category="label">
+                {formatBlockchainAddress(getWalletContractAddress())}
+              </Text>
+            </BlurView>
+          </TouchableWithoutFeedback>
         </View>
-      </ScrollView>
+      </View>
+
+      <View>
+        <View
+          style={{
+            width: '100%',
+            height: 1000,
+            marginTop: 56 * vh,
+            paddingTop: 8 * vh,
+          }}
+        >
+          <Text>Ciao</Text>
+        </View>
+      </View>
     </Layout>
   )
 }
 
 const themedStyles = StyleService.create({
   imageContainer: {
-    left: '-20%',
-    width: '140%',
+    position: 'absolute',
+    top: 0,
+    left: -20 * vw,
+    zIndex: 1,
+    height: 60 * vh,
+    width: 140 * vw,
+  },
+  imageBackground: {
+    ...appStyles.center,
     borderBottomLeftRadius: 150,
     borderBottomRightRadius: 150,
     overflow: 'hidden',
@@ -110,6 +97,7 @@ const themedStyles = StyleService.create({
     shadowRadius: 10,
     elevation: 15,
   },
+
   balanceContainer: {
     width: 50 * vw,
     height: 10 * vh,
@@ -124,11 +112,12 @@ const themedStyles = StyleService.create({
     color: 'white',
   },
   addressContainer: {
+    position: 'absolute',
+    bottom: -4 * vh,
+    left: 50 * vw,
+    zIndex: 2,
     width: 40 * vw,
     height: 8 * vh,
-    position: 'absolute',
-    top: -4 * vh,
-    left: 30 * vw,
     borderRadius: 10,
     overflow: 'hidden',
     borderColor: 'color-primary-400',
