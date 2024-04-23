91e2bef074160c824c91f819c990e12196c9c92e
Nolli Manuele <manuele.nolli@supsi.ch>
2024-04-23 14:55:45 +0200

    Added SapphireSecurityManager tests

--- blockchain/contracts/modules/SapphireSecurityManager.sol
+++ blockchain/contracts/modules/SapphireSecurityManager.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "./common/BaseModule.sol";
+import "./SecurityManager.sol";
 
 /**
  * @title SapphireSecurityManager
@@ -10,58 +11,94 @@ import "./common/BaseModule.sol";
  */
 abstract contract SapphireSecurityManager is BaseModule {
 
-    mapping(address => mapping(address => bool)) private _guardians; // wallet => guardian => isGuardian
-
     // *************** Events *************************** //
 
+    event RecoveryFinalized(address indexed wallet, address indexed _recovery);
     event GuardianAdded(address indexed wallet, address indexed guardian);
-    event GuardianRemoved(address indexed wallet, address indexed guardian);
+    event GuardianRevoked(address indexed wallet, address indexed guardian);
 
-    // *************** Modifier ************************ //
+    // *************** Modifiers ************************ //
 
-    modifier onlyGuardian(address _wallet) {
-        require(_guardians[_wallet][msg.sender], "SapphireSecurityManager: caller is not a guardian");
+    /**
+     * @notice Throws if the caller is not a guardian for the wallet or the module itself.
+     */
+    modifier onlyWalletOwnerOrGuardianOrSelf(address _wallet) {
+        require(_isOwner(_wallet, msg.sender) || _isSelf(msg.sender) || isGuardian(_wallet, msg.sender), "SSM: must be owner/guardian/self");
         _;
     }
 
     // *************** External functions ************************ //
 
     /**
-     * @notice Adds a guardian to the wallet.
-     * @param _wallet The wallet address.
-     * @param _guardian The guardian address.
+     * @notice Lets the owner add a guardian to its wallet.
+     * @param _wallet The target wallet.
+     * @param _guardian The guardian to add.
      */
-    function addGuardian(address _wallet, address _guardian) external onlyWalletOwnerOrSelf(_wallet) onlyWhenUnlocked(_wallet) {
-        _guardians[_wallet][_guardian] = true;
+    function addGuardian(address _wallet, address _guardian) external onlyWalletOwnerOrSelf(_wallet) {
+        require(!_isOwner(_wallet, _guardian), "SSM: guardian cannot be owner");
+        require(!isGuardian(_wallet, _guardian), "SSM: duplicate guardian");
+        // Guardians must either be an EOA or a contract with an owner()
+        // method that returns an address with a 25000 gas stipend.
+        // Note that this test is not meant to be strict and can be bypassed by custom malicious contracts.
+        (bool success,) = _guardian.call{gas: 25000}(abi.encodeWithSignature("owner()"));
+        require(success, "SSM: must be EOA/Argent wallet");
+
+        guardianStorage.addGuardian(_wallet, _guardian);
         emit GuardianAdded(_wallet, _guardian);
     }
 
     /**
-     * @notice Removes a guardian from the wallet.
-     * @param _wallet The wallet address.
-     * @param _guardian The guardian address.
+     * @notice Lets the owner revoke a guardian from its wallet.
+     * @param _wallet The target wallet.
+     * @param _guardian The guardian to revoke.
      */
-    function removeGuardian(address _wallet, address _guardian) external onlyWalletOwnerOrSelf(_wallet) onlyWhenUnlocked(_wallet) {
-        _guardians[_wallet][_guardian] = false;
-        emit GuardianRemoved(_wallet, _guardian);
+    function revokeGuardian(address _wallet, address _guardian) external onlyWalletOwnerOrSelf(_wallet) {
+        require(isGuardian(_wallet, _guardian), "SSM: must be existing guardian");
+
+        guardianStorage.revokeGuardian(_wallet, _guardian);
+        emit GuardianRevoked(_wallet, _guardian);
     }
 
-    function recoverWallet(address _wallet, address _newOwner) external onlyGuardian(_wallet) onlyWhenUnlocked(_wallet) {
-        require(_newOwner != address(0), "SapphireSecurityManager: new owner is the zero address");
+    /**
+     * @notice Lets the guardians start the execution of the recovery procedure.
+     * Once triggered the recovery is pending for the security period before it can be finalised.
+     * Must be confirmed by N guardians, where N = ceil(Nb Guardians / 2).
+     * @param _wallet The target wallet.
+     * @param _recovery The address to which ownership should be transferred.
+     */
+    function executeRecovery(address _wallet, address _recovery) external onlyWalletOwnerOrGuardianOrSelf(_wallet) {
+        validateNewOwner(_wallet, _recovery);
         _clearSession(_wallet);
-        IWallet(_wallet).setOwner(_newOwner);
+        IWallet(_wallet).setOwner(_recovery);
+        emit RecoveryFinalized(_wallet, _recovery);
     }
+
+    /**
+     * @notice Get the active guardians for a wallet.
+     * @param _wallet The target wallet.
+     * @return _guardians the active guardians for a wallet.
+     */
+    function getGuardians(address _wallet) external view returns (address[] memory _guardians) {
+        return guardianStorage.getGuardians(_wallet);
+    }
+
 
     // *************** Public functions ************************ //
 
-    /**
-     * @notice Checks if a guardian is authorised on the wallet.
-     * @param _wallet The wallet address.
-     * @param _guardian The guardian address.
-     * @return `true` if the guardian is authorised, otherwise `false`.
+        /**
+     * @notice Checks if an address is a guardian for a wallet.
+     * @param _wallet The target wallet.
+     * @param _guardian The address to check.
+     * @return _isGuardian `true` if the address is a guardian for the wallet otherwise `false`.
      */
-    function isGuardian(address _wallet, address _guardian) public view returns (bool) {
-        return _guardians[_wallet][_guardian];
+    function isGuardian(address _wallet, address _guardian) public view returns (bool _isGuardian) {
+        return guardianStorage.isGuardian(_wallet, _guardian);
     }
 
+    // *************** Internal Functions ********************* //
+
+    function validateNewOwner(address _wallet, address _newOwner) internal view {
+        require(_newOwner != address(0), "SSM: new owner cannot be null");
+        require(!isGuardian(_wallet, _newOwner), "SSM: new owner cannot be guardian");
+    }
 }
--- blockchain/test/modules/SapphireSecurityManager.ts
+++ blockchain/test/modules/SapphireSecurityManager.ts
@@ -1,48 +1,146 @@
 import { ethers } from 'hardhat'
-import { expect } from 'chai'
-import { ArgentModule, SapphireSecurityManager, SapphireSecurityManager__factory } from '../../typechain-types'
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers'
 import deployInfrastructure from '../../scripts/argentContracts/deployInfrastructure'
 import { InfrastructureTypes } from '../../scripts/argentContracts/utils/infrastructureTypes'
+import { createWallet } from '../../scripts/argentContracts/createWallet'
+import { expect } from 'chai'
+import { ZeroAddress } from 'ethers'
+import { IWallet__factory } from '../../typechain-types'
 
 describe('SapphireSecurityManager', function () {
   let deployer: HardhatEthersSigner
   let walletOwner: HardhatEthersSigner
   let guardian1: HardhatEthersSigner
   let guardian2: HardhatEthersSigner
+  let newOwner: HardhatEthersSigner
   let walletAccountAbstraction: string
   let infrastructure: InfrastructureTypes
 
   beforeEach(async function () {
-    ;[deployer, walletOwner, guardian1, guardian2] = await ethers.getSigners()
+    ;[deployer, walletOwner, guardian1, guardian2, newOwner] = await ethers.getSigners()
     infrastructure = await deployInfrastructure(deployer)
 
     // create walletAccountAbstraction
+    walletAccountAbstraction = await createWallet(
+      infrastructure.walletFactory,
+      walletOwner.address,
+      guardian1.address,
+      deployer.address,
+      await infrastructure.argentModule.getAddress()
+    )
+  })
+
+  describe('createWallet', async function () {
+    it('Should get the constructor guardian', async function () {
+      const isGuardian = await infrastructure.argentModule.isGuardian(walletAccountAbstraction, guardian1.address)
+      expect(isGuardian).to.be.true
+
+      const guardianList = await infrastructure.argentModule.getGuardians(walletAccountAbstraction)
+      expect(guardianList.length).to.be.equal(1)
+      expect(guardianList[0]).to.be.equal(guardian1.address)
+    })
   })
 
   describe('addGuardian', async function () {
-    it('Should allow the wallet owner to add a guardian', async function () {})
-  })
+    it('Should allow the wallet owner to add a guardian', async function () {
+      // Add guardian2
+      await infrastructure.argentModule.connect(walletOwner).addGuardian(walletAccountAbstraction, guardian2.address)
 
-  describe('removeGuardian', async function () {
-    beforeEach(async function () {
-      // Add guardian1
+      const isGuardian = await infrastructure.argentModule.isGuardian(walletAccountAbstraction, guardian2.address)
+      expect(isGuardian).to.be.true
+
+      const guardianList = await infrastructure.argentModule.getGuardians(walletAccountAbstraction)
+      expect(guardianList.length).to.be.equal(2)
+      expect(guardianList[1]).to.be.equal(guardian2.address)
     })
 
-    it('Should allow the wallet owner to remove a guardian', async function () {})
-  })
-
-  describe('recoverWallet', async function () {
-    beforeEach(async function () {
-      // Add guardian1
+    it('Should emit a GuardianAdded event', async function () {
+      await expect(infrastructure.argentModule.connect(walletOwner).addGuardian(walletAccountAbstraction, guardian2.address))
+        .to.emit(infrastructure.argentModule, 'GuardianAdded')
+        .withArgs(walletAccountAbstraction, guardian2.address)
     })
 
-    it('Should allow a guardian to recover the wallet', async function () {})
+    it('Should revert if the guardian is already a guardian', async function () {
+      await expect(infrastructure.argentModule.connect(walletOwner).addGuardian(walletAccountAbstraction, guardian1.address)).to.be.revertedWith(
+        'SSM: duplicate guardian'
+      )
+    })
 
-    it('Should revert if the new owner is the zero address', async function () {})
+    it('Should revert if the guardian is the owner', async function () {
+      await expect(infrastructure.argentModule.connect(walletOwner).addGuardian(walletAccountAbstraction, walletOwner.address)).to.be.revertedWith(
+        'SSM: guardian cannot be owner'
+      )
+    })
 
-    it('Should revert if called by a non-guardian', async function () {})
+    it('Should revert if the caller is not the owner', async function () {
+      await expect(infrastructure.argentModule.connect(guardian1).addGuardian(walletAccountAbstraction, guardian2.address)).to.be.revertedWith(
+        'BM: must be wallet owner/self'
+      )
+    })
+  })
 
-    it('Should revert if called when the wallet is locked', async function () {})
+  describe('revokeGuardian', async function () {
+    it('Should allow the wallet owner to remove a guardian', async function () {
+      // Remove guardian1
+      await infrastructure.argentModule.connect(walletOwner).revokeGuardian(walletAccountAbstraction, guardian1.address)
+
+      const isGuardian = await infrastructure.argentModule.isGuardian(walletAccountAbstraction, guardian1.address)
+      expect(isGuardian).to.be.false
+
+      const guardianList = await infrastructure.argentModule.getGuardians(walletAccountAbstraction)
+      expect(guardianList.length).to.be.equal(0)
+    })
+
+    it('Should emit a GuardianRevoked event', async function () {
+      await expect(infrastructure.argentModule.connect(walletOwner).revokeGuardian(walletAccountAbstraction, guardian1.address))
+        .to.emit(infrastructure.argentModule, 'GuardianRevoked')
+        .withArgs(walletAccountAbstraction, guardian1.address)
+    })
+
+    it('Should revert if the guardian is not a guardian', async function () {
+      await expect(infrastructure.argentModule.connect(walletOwner).revokeGuardian(walletAccountAbstraction, guardian2.address)).to.be.revertedWith(
+        'SSM: must be existing guardian'
+      )
+    })
+  })
+
+  describe('executeRecovery', async function () {
+    it('Should allow a guardian to recover the wallet', async function () {
+      // before owner
+      expect(await IWallet__factory.connect(walletAccountAbstraction, deployer).owner(), walletOwner.address)
+
+      // Recover wallet
+      await infrastructure.argentModule.connect(guardian1).executeRecovery(walletAccountAbstraction, newOwner)
+
+      const isGuardian = await infrastructure.argentModule.isGuardian(walletAccountAbstraction, guardian1.address)
+      expect(isGuardian).to.be.true // guardian1 is still a guardian
+
+      // after owner
+      expect(await IWallet__factory.connect(walletAccountAbstraction, deployer).owner(), newOwner.address)
+    })
+
+    it('Should emit a Recovery event', async function () {
+      await expect(infrastructure.argentModule.connect(guardian1).executeRecovery(walletAccountAbstraction, newOwner))
+        .to.emit(infrastructure.argentModule, 'RecoveryFinalized')
+        .withArgs(walletAccountAbstraction, newOwner.address)
+    })
+
+    it('Should revert if the new owner is the zero address', async function () {
+      await expect(infrastructure.argentModule.connect(guardian1).executeRecovery(walletAccountAbstraction, ZeroAddress)).to.be.revertedWith(
+        'SSM: new owner cannot be null'
+      )
+    })
+
+    it('Should revert if the new owner is a guardian', async function () {
+      await expect(infrastructure.argentModule.connect(guardian1).executeRecovery(walletAccountAbstraction, guardian1.address)).to.be.revertedWith(
+        'SSM: new owner cannot be guardian'
+      )
+    })
+
+    it('Should revert if called by a non-guardian', async function () {
+      await expect(infrastructure.argentModule.connect(guardian2).executeRecovery(walletAccountAbstraction, newOwner)).to.be.revertedWith(
+        'SSM: must be owner/guardian/self'
+      )
+    })
   })
 })
