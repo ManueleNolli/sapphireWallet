d5b81c06a7ac52719e987067e7d133531e3a6488
Nolli Manuele <manuele.nolli@supsi.ch>
2024-06-07 13:12:43 +0200

    Update style

--- bridge/basicOffChainBridge/src/handleBridgeNFT.ts
+++ bridge/basicOffChainBridge/src/handleBridgeNFT.ts
@@ -5,30 +5,24 @@ import { ethers } from 'ethers'
 async function handleBridgeNFT(argentWrappedAccountsAddress: string, wallet: string, originalNFTAddress: string, data: string, baseChainSigner: ethers.Signer,destChainSigner: ethers.Signer) {
   // Parse the data
   const [, , tokenId] = IERC721__factory.createInterface().decodeFunctionData('safeTransferFrom(address,address,uint256)', data)
-  console.log('tokenId', tokenId)
 
   // Check if the token supports ERC721URIStorage
   try {
   const tokenUriSupport = await IERC721__factory.connect(originalNFTAddress, baseChainSigner).supportsInterface('0x5b5e139f')
-    console.log('tokenUriSupport', tokenUriSupport)
   if (!tokenUriSupport) {
     console.error('handleBridgeNFT: NFT does not support tokenURI, it cannot be bridged')
     return
   }} catch (e) {
-    console.error('ERROR boh', e)
+    console.error('ERROR', e)
   }
 
   const tokenURI = await ERC721URIStorage__factory.connect(originalNFTAddress, baseChainSigner).tokenURI(tokenId)
-  console.log('tokenURI', tokenURI)
 
   // Mint the NFT
   const argentWrappedAccounts = ArgentWrappedAccounts__factory.connect(argentWrappedAccountsAddress, destChainSigner)
-  console.log('Minting NFT...')
   const tx = await argentWrappedAccounts.safeMint(wallet, tokenURI, originalNFTAddress, tokenId)
-  console.log('Minted NFT')
 
   const receipt = await tx.wait()
-  console.log('receipt', receipt)
 
   if (receipt == null || receipt.blockNumber == null) {
     console.error('Transaction failed')
